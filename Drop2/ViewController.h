948498e6be50f19e5bb4e91c88b8004592eb9a30
mezhevikin <mezhevikin@gmail.com>
2013-07-02 16:56:44 +0400

    Initial Commit

---
+++ Drop2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Drop2
+//
+//  Created by Администратор on 7/2/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Drop2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Drop2
+//
+//  Created by Администратор on 7/2/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
