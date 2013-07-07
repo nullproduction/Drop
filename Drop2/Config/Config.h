57a84b91d0894e17f379926c68d08717ac23093e
mezhevikin <mezhevikin@gmail.com>
2013-07-07 16:23:45 +0400

    all

---
+++ Drop2/Config/Config.h
@@ -0,0 +1,26 @@
+/*
+ * Core data
+ */
+
+// Dropbox key and secret
+#define kTICDDropboxSyncKey @"f5o28idtrkxzb14"
+#define kTICDDropboxSyncSecret @"a7cm88dd6jpesm6"
+
+// App Identifier
+#define kGlobalAppIdentifier @"Drop2"
+
+// UDID key
+#define kSyncClientUUIDKey @"syncClientUUID"
+
+// Store core data
+#define kStoreURL @"Drop2.sqlite"
+#define kModelURL @"Drop2"
+
+// Document Identifier
+#define kDocumentIdentifier @"Notebook"
+
+// Encryption Password
+#define kEncryptionPassword @"0x1928b43"
+
+// Show activity sync
+#define kShowActivitySync NO
